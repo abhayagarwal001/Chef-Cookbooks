a0f33dfe16bd7437229c953355817651c8ff6065
abhay <abhayagarwal4483@gmail.com>
2022-11-09 14:47:37 +0000

    Contain recipe to create a file

---
+++ test-cookbook/recipes/test-recipe1.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: test-cookbook
+# Recipe:: test-recipe1
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+  file '/myfile' do
+  content 'This is Abhay Learning Chef Tool !!'
+  action :create
+  end
+
