cbf6a2e0135556e928f6032e741afb91f7ccd2f3
abhay <abhayagarwal4483@gmail.com>
2022-11-09 15:26:56 +0000

    Recipe to install tree package and create file

---
+++ test-cookbook/recipes/recipe2.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: test-cookbook
+# Recipe:: recipe2
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+  package 'tree' do
+  action :install
+  end
+
+  file '/myfile2' do
+  content "this is second recipe"
+  action :create
+
+  end
